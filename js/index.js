5e45816fdaed77877f7314299025902bd41e7d24
Andrey <tymoshenko.andrey@gmail.com>
2020-07-31 16:04:51 +0300

    Added C & CE buttons

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
-const display = document.querySelector(".display");
+const display = document.querySelector('.display');
 
 document
-  .querySelectorAll(".digits button")
-  .forEach((button) => button.addEventListener("click", digitPressed));
+  .querySelectorAll('.digits button')
+  .forEach((button) => button.addEventListener('click', digitPressed));
 
 function digitPressed(ev) {
   const digit = ev.target.innerText;
@@ -10,16 +10,39 @@ function digitPressed(ev) {
 }
 
 document
-  .querySelectorAll(".opers button")
-  .forEach((button) => button.addEventListener("click", operPressed));
+  .querySelectorAll('.opers button')
+  .forEach((button) => button.addEventListener('click', operPressed));
 
 function operPressed(ev) {
   const oper = ev.target.innerText;
   display.value += oper;
 }
 
-document.querySelector(".eq").addEventListener("click", eqPressed);
+document.querySelector('.eq').addEventListener('click', eqPressed);
 
 function eqPressed() {
   display.value = eval(display.value);
+}
+
+document.querySelector('.clear').addEventListener('click', clearPressed);
+
+function clearPressed(e) {
+  const clear = e.target.innerText;
+  display.value = ' ';
+}
+
+document
+  .querySelector('.clear-empty')
+  .addEventListener('click', clearEmptyPressed);
+
+function clearEmptyPressed(e) {
+  const ce = e.target.innerText;
+  display.value = display.value.substring(0, display.value.length - 1);
+}
+
+document.querySelector('.point').addEventListener('click', pointPressed);
+
+function pointPressed(ev) {
+  const point = ev.target.innerText;
+  display.value += point;
 }
